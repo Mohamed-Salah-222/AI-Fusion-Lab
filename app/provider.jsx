9371c9df2e006443c610652356273a51ffd14bfe
SilverCrow222 <midosalah25552@gmail.com>
2025-09-20 19:54:38 +0300

    Installed shadcn & Dark Theme

---
+++ app/provider.jsx
@@ -0,0 +1,11 @@
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+function Provider({ children, ...props }) {
+  return (
+    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange {...props}>
+      <div>{children}</div>
+    </NextThemesProvider>
+  );
+}
+
+export default Provider;
