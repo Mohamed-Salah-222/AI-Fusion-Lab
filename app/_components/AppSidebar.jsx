72627aab239772d9a50d8a404f57fed47810182f
SilverCrow222 <midosalah25552@gmail.com>
2025-09-21 19:58:49 +0300

    Added Authentication

--- app/_components/AppSidebar.jsx
+++ app/_components/AppSidebar.jsx
@@ -2,12 +2,15 @@
 
 import { Button } from "@/components/ui/button";
 import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarHeader } from "@/components/ui/sidebar";
-import { Moon, Sun } from "lucide-react";
+import { SignIn, SignInButton, useUser } from "@clerk/nextjs";
+import { Bolt, Moon, Sun, User2, Zap } from "lucide-react";
 import { useTheme } from "next-themes";
 import Image from "next/image";
+import UsageCreditProgress from "./UsageCreditProgress";
 
 export function AppSidebar() {
   const { theme, setTheme } = useTheme();
+  const { user } = useUser();
   return (
     <Sidebar>
       <SidebarHeader>
@@ -29,24 +32,40 @@ export function AppSidebar() {
               )}
             </div>
           </div>
-          <Button className="mt-7 w-full" size="lg">
-            + New Chat
-          </Button>
+          {user?
+          <Button className="mt-7 w-full" size="lg">+ New Chat</Button> :
+          <SignInButton>
+          <Button className="mt-7 w-full" size="lg">+ New Chat</Button> 
+          </SignInButton>}
         </div>
       </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
           <div className={"p-3"}>
             <h2 className="font-bold text-lg">Chat</h2>
-            <p className="text-sm text-gray-400">Sign in to start chating with multiple AI models</p>
+          {!user &&  <p className="text-sm text-gray-400">Sign in to start chating with multiple AI models</p> }
           </div>
         </SidebarGroup>
       </SidebarContent>
       <SidebarFooter>
         <div className="p-3 mb-10">
-          <Button className={"w-full"} size={"lg"}>
-            Sign In / Sign Up
-          </Button>
+          {!user ? (
+            <SignInButton mode="modal">
+              <Button className={"w-full"} size={"lg"}>
+                Sign In / Sign Up
+              </Button>
+            </SignInButton>
+          ) : (
+            <div>
+              <UsageCreditProgress />
+              <Button className={"w-full"}>
+                <Zap /> Upgrade Plan
+              </Button>
+              <Button className="flex w-full " variant={"ghost"}>
+                <User2 /> <h2>Settings</h2>
+              </Button>
+            </div>
+          )}
         </div>
       </SidebarFooter>
     </Sidebar>
--- app/provider.jsx
+++ app/provider.jsx
@@ -1,9 +1,45 @@
+"use client";
+
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { AppSidebar } from "./_components/AppSidebar";
 import AppHeader from "./_components/AppHeader";
+import { useUser } from "@clerk/nextjs";
+import { useEffect } from "react";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { db } from "@/config/FirebaseConfig";
 
 function Provider({ children, ...props }) {
+  const { user } = useUser();
+
+  useEffect(() => {
+    if (user) {
+      CreateNewuser();
+    }
+  }, [user]);
+
+  const CreateNewuser = async () => {
+    const userRef = doc(db, "users", user?.primaryEmailAddress?.emailAddress);
+
+    const userSnap = await getDoc(userRef);
+
+    if (userSnap.exists()) {
+      console.log("Exitiing User");
+      return;
+    } else {
+      const userData = {
+        name: user?.fullName,
+        email: user?.primaryEmailAddress?.emailAddress,
+        createdAt: new Date(),
+        remainingMsg: 5,
+        plan: "Free",
+        credits: 1000,
+      };
+      await setDoc(userRef, userData);
+      console.log("New User data saved");
+    }
+  };
+
   return (
     <NextThemesProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange {...props}>
       <SidebarProvider>
